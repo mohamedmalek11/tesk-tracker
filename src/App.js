986c92ce451fb930d57008938ecb4bfe949ecb1b
mohamedmalek11 <mohamad.sh.msm@gmail.com>
2022-04-29 01:27:40 +0200

    Created task component

--- src/App.js
+++ src/App.js
@@ -27,10 +27,17 @@ function App() {
     ]
   )
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) =>  task.id === id ? { ...task, reminder: !task.reminder } : task ))
+  }
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No Tasks For Today"}
     </div>
   );
 }
