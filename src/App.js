9124e71eacfb51989aae5e33f8a753e317cf71d8
mohamedmalek11 <mohamad.sh.msm@gmail.com>
2022-04-28 02:00:21 +0200

    Created tasks component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { useState } from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
-const name = 'malek'
+
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: "dummy1",
+        day: "feb 3 at 5:30pm",
+        reminder: false,
+      },
+      {
+        id: 2,
+        text: "dummy2",
+        day: "feb 4 at 2:30pm",
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: "dummy3",
+        day: "feb 6 at 1:30pm",
+        reminder: false,
+      }
+    ]
+  )
 
   return (
     <div className="container">
       <Header />
-   
+      <Tasks tasks={tasks} />
     </div>
   );
 }
